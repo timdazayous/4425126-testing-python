5bd2236b76424a53bb468374893dc68aa390a8f4
timdazayous <tim.dazayous@gmail.com>
2025-11-19 16:45:55 +0100

    All add testing

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -12,7 +12,7 @@ def test_make_simple_addition():
     operation = "10 + 5"
     expected_value = 15
     assert sut.addition(operation) == expected_value
-    
+
 # Ce qu'il devrait se passer
 def test_should_negatives_numbers_addition():
     sut = Operators()
@@ -30,4 +30,34 @@ def test_random_spaces_addition():
     sut = Operators()
     operation = "   5+  7  "
     expected_value = 12
+    assert sut.addition(operation) == expected_value
+
+def test_should_make_float_addition():
+    sut = Operators()
+    operation = "0.5 + 1.5"
+    expected_value = 2
+    assert sut.addition(operation) == expected_value
+
+def test_should_make_multiple_float_addition():
+    sut = Operators()
+    operation = "0.5 + 1.5 + 2.0 + 2.5"
+    expected_value = 6.5
+    assert sut.addition(operation) == expected_value
+
+def test_invalid_addition():
+    sut = Operators()
+    operation = "a + 2"
+    expected_value = None
+    assert sut.addition(operation) == expected_value
+
+def test_no_sign_addition():
+    sut = Operators()
+    operation = "42"
+    expected_value = 42.0
+    assert sut.addition(operation) == expected_value
+
+def test_no_sign2_addition():
+    sut = Operators()
+    operation = "4 2"
+    expected_value = None
     assert sut.addition(operation) == expected_value
