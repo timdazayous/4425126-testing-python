9ee8701b317ddb500ca74db613b8ad701f758c90
timdazayous <tim.dazayous@gmail.com>
2025-11-20 10:36:37 +0100

    Test operators et test view done

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,5 +1,6 @@
 from calculate.operators import Operators
 
+# test d'addition
 def test_should_make_multiple_addition():
     # Création d'un objet de la classe operator pour pouvoir y appliquer ses fonctions
     sut = Operators()
@@ -61,3 +62,132 @@ def test_no_sign2_addition():
     operation = "4 2"
     expected_value = None
     assert sut.addition(operation) == expected_value
+
+# test substraction
+def test_standard_substraction():
+    sut = Operators()
+    operation = "10 - 5"
+    expected_value = 5
+    assert sut.substraction(operation) == expected_value
+
+def test_multiple_substraction():
+    sut = Operators()
+    operation = "10 - 5 - 3"
+    expected_value = 2
+    assert sut.substraction(operation) == expected_value
+
+def test_float_substraction():
+    sut = Operators()
+    operation = "10.5 - 5.5"
+    expected_value = 5
+    assert sut.substraction(operation) == expected_value
+
+def test_negative_num_substraction():
+    sut = Operators()
+    operation = "10 - -5"
+    expected_value = 15
+    assert sut.substraction(operation) == expected_value
+
+def test_invalid_input_substraction():
+    sut = Operators()
+    operation = "10 - abc"
+    expected_value = None
+    assert sut.substraction(operation) == expected_value
+
+def test_no_sign_substraction():
+    sut = Operators()
+    operation = "42"
+    expected_value = 42.0
+    assert sut.substraction(operation) == expected_value
+
+# test multiplication
+def test_standard_multiplication():
+    sut = Operators()
+    operation = "10 * 5"
+    expected_value = 50
+    assert sut.multiplication(operation) == expected_value
+
+def test_multiple_multiplication():
+    sut = Operators()
+    operation = "10 * 5 * 2"
+    expected_value = 100
+    assert sut.multiplication(operation) == expected_value
+
+def test_float_multiplication():
+    sut = Operators()
+    operation = "2.5 * 4"
+    expected_value = 10
+    assert sut.multiplication(operation) == expected_value
+
+def test_by_zero_multiplication():
+    sut = Operators()
+    operation = "10 * 0 * 2"
+    expected_value = 0
+    assert sut.multiplication(operation) == expected_value
+
+def test_invalid_input_multiplication():
+    sut = Operators()
+    operation = "10 * abc"
+    expected_value = None
+    assert sut.multiplication(operation) == expected_value
+
+def test_no_sign_multiplication():
+    sut = Operators()
+    operation = "105"
+    expected_value = 105
+    assert sut.multiplication(operation) == expected_value
+
+def test_solo_multiplication():
+    sut = Operators()
+    operation = "10"
+    expected_value = 10
+    assert sut.multiplication(operation) == expected_value
+
+# test division
+def test_simple_division():
+    sut = Operators()
+    operation = "10 / 5"
+    expected_value = 2
+    assert sut.division(operation) == expected_value
+
+def test_simple_2_division():
+    sut = Operators()
+    operation = "10/5"
+    expected_value = 2
+    assert sut.division(operation) == expected_value
+
+def test_multiple_division():
+    sut = Operators()
+    operation = "10 / 2 / 2"
+    expected_value = 2.5
+    assert sut.division(operation) == expected_value
+
+def test_float_division():
+    sut = Operators()
+    operation = "2.4 / 2"
+    expected_value = 1.2
+    assert sut.division(operation) == expected_value
+
+def test_float_2_division():
+    sut = Operators()
+    operation = "10 / 2.5"
+    expected_value = 4
+    assert sut.division(operation) == expected_value
+
+def test_by_zero_division():
+    sut = Operators()
+    operation = "10 / 0"
+    expected_value = None
+    assert sut.division(operation) == expected_value
+
+def test_solo_division():
+    sut = Operators()
+    operation = "10"
+    expected_value = 10
+    assert sut.division(operation) == expected_value
+
+def test_invalid_input_division():
+    sut = Operators()
+    operation = "10 / abc"
+    expected_value = None
+    assert sut.division(operation) == expected_value
